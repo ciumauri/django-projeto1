15ee5e7e9a57e1c676abf18e96f9adac0c5a0924
Mauricio Oliveira <ciumauri@gmail.com>
2022-04-12 11:30:09 -0300

    Inclusão dos STATICFILES

--- projeto1/settings.py
+++ projeto1/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             BASE_DIR / 'base_templates',
-            ],
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+    BASE_DIR / 'base_template',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
