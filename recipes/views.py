8dbbcf718bc1a015550233cbb28723f0b6230ec3
Mauricio Oliveira <ciumauri@gmail.com>
2022-04-13 06:45:13 -0300

    Injetando dados na lista da view Home

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Mauricio Oliveira',
+        'recipes': [make_recipe() for _ in range(9)],
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Mauricio Oliveira',
+        'recipe': make_recipe(),
     })
