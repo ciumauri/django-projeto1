a1c9eb48c1b5718547ff95cb0a42adfb636e17f8
Mauricio Oliveira <ciumauri@gmail.com>
2022-04-14 15:25:53 -0300

    Filtrando is_published

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,5 @@
 from distutils.command.upload import upload
+from email.policy import default
 from multiprocessing.spawn import prepare
 from operator import mod
 from turtle import title, update
@@ -29,7 +30,8 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    cover = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(
