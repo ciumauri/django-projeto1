04c800bdc7f2b4582f5e8fd851c0147b79f1a416
Mauricio Oliveira <ciumauri@gmail.com>
2022-04-18 16:52:25 -0300

    Iniciando com testes no Django com Pytest, Pytest-Django e Unittest

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class RecipeUrlsTest(TestCase):
+    def test_the_pypest_is_ok(self):
+        print('Ola mundo')
+        assert 1 == 1, 'Um é igual a um'
